4080e20d39ea97b643e0789b7236b06859535da5
Rain Thompson <60662590+rainthompson@users.noreply.github.com>
2021-05-30 15:58:01 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,31 @@
+var character = 
+document.getElementById("character");
+var block =
+document.getElementById("block");
+
+
+function jump(){
+    
+    if(character.classList !="animate"){
+        character.classList.add("animate");
+    }
+    setTimeout(function(){
+        character.classList.remove("animate");
+    },500);
+    
+}
+
+var CheckLost = setInterval(function(){
+    var characterTop = 
+    parseInt (window.getComputedStyle(character)
+   .getPropertyValue("top"));
+   var blockLeft = 
+    parseInt (window.getComputedStyle(block)
+   .getPropertyValue("left"));
+   if (blockLeft<20 && blockLeft > 0
+     && characterTop >= 130){
+         block.style.animation = "none";
+         alert("SORRY, PLAY AGAIN");
+     } 
+    
+},10);
